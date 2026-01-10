e8424abfd7f43bda4d6859fb2acce289285e89a1
Ramakrishna-K <ramakrishnakatravth0@gmail.com>
2026-01-10 11:57:14 +0530

    while_loops

---
+++ while_loops.js
@@ -0,0 +1,15 @@
+let fuel =10;
+
+while (fuel <= 10){
+    console.log("filling fuel...");
+    fuel ++
+}
+console.log("thanks your tank is full")
+
+
+let otpCorrect = false;
+
+while (!otpCorrect){
+    console.log("Waiting for correct OTP....")
+    otpCorrect = true
+}
